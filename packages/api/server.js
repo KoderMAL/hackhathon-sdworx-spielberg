8b9e894787ce3542ceb56b3ae6bb0b2cf1baf147
Amine Lalaoui <amine_lalaoui@hotmail.com>
2021-02-06 23:10:33 +0100

    Server mocking

--- packages/api/server.js
+++ packages/api/server.js
@@ -13,13 +13,21 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status(200).send('hello world');
+app.get('/me', (req, res) => {
+  return {
+    cheersReceived: 35,
+    cheersGiven: 45,
+    cheersSpent: 11,
+    badges: [{ "Mentoring": 12 },
+      { "Seniority": 3 },
+      { "Ponctuality": 23 },
+      { "Run With Colleagues": 13 }],
+    team: "IT"
+  }
 });
 
 app.listen(config.port, config.host, (e)=> {
     if(e) {
         throw new Error('Internal Server Error');
     }
-    console.info(`${config.name} running on ${config.host}:${config.port}`);
 });
